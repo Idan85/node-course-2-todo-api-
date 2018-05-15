99b499b84e421475b66d1a5146a9678d5ddfc801
Idan <newuser@gmail.com>
2018-05-16 00:40:25 +0300

    Setup seprate test and development envs

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ var env = process.env.NODE_ENV || 'development';
 
 //console.log('env *****', env);
 
-if (env === 'production') {
+if (env === 'development') {
     
     process.env.PORT = 3000; 
 
